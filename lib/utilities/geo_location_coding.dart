29adaf69b3acc648998190968148258fd2ff7259
Sricharan Ramesh <sricharan.ramesh@pocketpills.com>
2023-01-31 10:54:32 +0530

    fix: geolocation permission issues

--- lib/utilities/geo_location_coding.dart
+++ lib/utilities/geo_location_coding.dart
@@ -4,22 +4,24 @@ import 'dart:developer' as dev;
 
 class GeoLocationCoding {
   late String _currentAddress = '';
-  Future<Position?> _getCurrentLocation() async {
-    dev.log("Getting position");
-    try {
-      final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.medium,
-        forceAndroidLocationManager: true,
-      );
-      dev.log(position.toString());
-      return position;
-    } catch (e) {
-      dev.log(e.toString());
+
+  Future<Position> _getCurrentLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
     }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
   }
 
   Future<String> getCurrentAddress() async {
-    if(_currentAddress != '') {
+    if (_currentAddress != '') {
       return _currentAddress;
     }
     dev.log("Getting address");
@@ -30,7 +32,8 @@ class GeoLocationCoding {
           await placemarkFromCoordinates(position.latitude, position.longitude);
 
       Placemark place = placemarks[0];
-      _currentAddress =  "${place.locality}, ${place.postalCode}, ${place.country}";
+      _currentAddress =
+          "${place.locality}, ${place.postalCode}, ${place.country}";
       return _currentAddress;
     } else {
       return 'Address Not Available';
